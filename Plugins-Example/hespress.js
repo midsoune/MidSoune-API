7557a12cced4a0255d984bcde6bcc58bf16841d0
MidSoune <75175804+midsoune@users.noreply.github.com>
2024-02-28 16:43:05 +0100

    Update hespress.js

--- Plugins-Example/hespress.js
+++ Plugins-Example/hespress.js
@@ -1,1 +1,13 @@
-
+import fetch from 'node-fetch';
+const handler = async (m) => {
+let res = await fetch('https://midsoune-b5be75a7ee15.herokuapp.com/api/hespress')
+  let data = await res.json();
+  const results = JSON.parse(data.result);
+  let message = '';
+  for(let result of results) {
+     message += `*العنوان:* ${result.title}\n*الرابط:* ${result.url}\n\n`;
+    }
+   await m.reply(message);
+};
+handler.command = ['hespress'];
+export default handler;
