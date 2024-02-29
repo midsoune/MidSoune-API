dbed63125293639c45cfd665aa4b4abd427cab6d
MidSoune <75175804+midsoune@users.noreply.github.com>
2024-02-29 18:24:53 +0100

    Update Mediafire.js

--- Plugins-Example/Mediafire.js
+++ Plugins-Example/Mediafire.js
@@ -3,6 +3,6 @@ const midsoune = async (m, {conn, text}) => {
 let res = await fetch(`https://midsoune-b5be75a7ee15.herokuapp.com/api/mediafire?url=${text}`)
   let data = await res.json();
   const results = data.result
- conn.sendFile(m.chat, results.link, '', `${results.title} [${results.size}]`, m);};
+ conn.sendFile(m.chat, results.link, '', `${results.title} [${results.size} ]`, m);};
 midsoune.command = ['mediafire'];
 export default midsoune;
