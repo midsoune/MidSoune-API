c97fb0fc293b5e831c8b56025825eeacee49b17c
MidSoune <75175804+midsoune@users.noreply.github.com>
2024-02-29 15:00:01 +0100

    Add D-Talamidi

--- Plugins-Example/talamidi.js
+++ Plugins-Example/talamidi.js
@@ -1,13 +1,25 @@
 import fetch from 'node-fetch';
-const handler = async (m, {text}) => {
-let res = await fetch(`https://midsoune-b5be75a7ee15.herokuapp.com/api/talamidi?drs=${encodeURIComponent(text)}`)
-  let data = await res.json();
-  const results = JSON.parse(data.result);
-  let message = '';
-  for(let result of results) {
-     message += `*الدرس:* ${result.title}\n*الرابط:* ${result.url}\n\n`;
-    }
-   await m.reply(message);
+const midsoune = async (m, {conn, text}) => {
+  if (text.startsWith('https://')) {
+    try {
+      let res = await fetch(`https://midsoune-b5be75a7ee15.herokuapp.com/api/dtalamidi?url=${encodeURIComponent(text)}`);
+      let data = await res.json();
+      const results = JSON.parse(data.result);
+      for (let i = 0; i < results.link.length; i++) {
+        await conn.sendFile(m.chat, results.link[i], results.title[i] + '.pdf', '', m);}
+    } catch (error) {
+      console.error("حدث خطأ أثناء جلب البيانات:", error);}
+  } else {
+    try {
+      let res = await fetch(`https://midsoune-b5be75a7ee15.herokuapp.com/api/talamidi?drs=${encodeURIComponent(text)}`)
+      let data = await res.json();
+      const results = JSON.parse(data.result);
+      let message = '';
+      for(let result of results) {
+         message += `*الدرس:* ${result.title}\n*الرابط:* ${result.link}\n\n`;}
+       await m.reply(message);
+    } catch (error) {
+      console.error("حدث خطأ أثناء جلب البيانات:", error);}}
 };
-handler.command = ['talamidi'];
-export default handler;
+midsoune.command = ['talamidi'];
+export default midsoune;
