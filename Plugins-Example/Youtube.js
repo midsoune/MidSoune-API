1b3281df2482f7fe53f3a3ff3d2b9303185c06b9
MidSoune <75175804+midsoune@users.noreply.github.com>
2024-03-03 21:43:55 +0100

    Update Youtube.js

--- Plugins-Example/Youtube.js
+++ Plugins-Example/Youtube.js
@@ -3,19 +3,21 @@ import fetch from 'node-fetch';
 const midsoune = async (m, {conn, text}) => {
 let res = await fetch(`https://midsoune-b5be75a7ee15.herokuapp.com/api/youtube?url=${text}`)
   let data = await res.json();
-  const res = data.video
-  await conn.sendFile(m.chat, res.result, 'video.mp4', '', m);
+    const res = data.video.url
+    const title = data.video.title
+  await conn.sendMessage(m.chat, { video: { url: res }, mimetype: 'video/mp4', fileName: title + '.mp4'}, { quoted: m })
 };
 midsoune.command = ['youtube', 'ytv'];
 export default midsoune;
 
 // Audio
 import fetch from 'node-fetch';
-const midsoune = async (m, {conn, text}) => {
-let res = await fetch(`https://midsoune-b5be75a7ee15.herokuapp.com/api/youtube?url=${text}`)
-  let data = await res.json();
-  const res = data.audio
- await conn.sendMessage(m.chat, { audio: { url: res.result }, mimetype: 'audio/mp4', fileName: res.title + '.mp3'}, { quoted: m })
+const midsoune = async (m, { conn, text }) => {
+    let results = await fetch(`https://midsoune-b5be75a7ee15.herokuapp.com/api/youtube?url=${text}`);
+    let data = await results.json();
+    const res = data.audio.url
+    const title = data.audio.title
+    await conn.sendMessage(m.chat, { audio: { url: res }, mimetype: 'audio/mp4', fileName: `${title}.mp3`}, {quoted: m})
 };
 midsoune.command = ['ytmp3', 'yta'];
 export default midsoune;
