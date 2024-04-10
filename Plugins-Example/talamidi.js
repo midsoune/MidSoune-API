13b6f51fca9afad17f527d4757f50c89c3982e23
MidSoune <75175804+midsoune@users.noreply.github.com>
2024-04-10 00:04:42 +0000

    Update talamidi.js

--- Plugins-Example/talamidi.js
+++ Plugins-Example/talamidi.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 const midsoune = async (m, {conn, text}) => {
   if (text.startsWith('https://')) {
     try {
-      let res = await fetch(`https://midsoune-b5be75a7ee15.herokuapp.com/api/dtalamidi?url=${encodeURIComponent(text)}`);
+      let res = await fetch(`https://midsouneapi-fee7b0be8faf.herokuapp.com/api/dtalamidi?url=${encodeURIComponent(text)}`);
       let data = await res.json();
       const results = JSON.parse(data.result);
       for (let i = 0; i < results.link.length; i++) {
@@ -11,7 +11,7 @@ const midsoune = async (m, {conn, text}) => {
       console.error("حدث خطأ أثناء جلب البيانات:", error);}
   } else {
     try {
-      let res = await fetch(`https://midsoune-b5be75a7ee15.herokuapp.com/api/talamidi?drs=${encodeURIComponent(text)}`)
+      let res = await fetch(`https://midsouneapi-fee7b0be8faf.herokuapp.com/api/talamidi?drs=${encodeURIComponent(text)}`)
       let data = await res.json();
       const results = JSON.parse(data.result);
       let message = '';
