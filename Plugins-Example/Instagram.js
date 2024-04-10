7d0fcebf27ad725de6e6605085b5dfc660692c8c
MidSoune <75175804+midsoune@users.noreply.github.com>
2024-04-10 00:06:12 +0000

    Update Instagram.js

--- Plugins-Example/Instagram.js
+++ Plugins-Example/Instagram.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 const midsoune = async (m, {conn, text}) => {
-let res = await fetch(`https://midsoune-b5be75a7ee15.herokuapp.com/api/igdl?url=${text}`)
+let res = await fetch(`https://midsouneapi-fee7b0be8faf.herokuapp.com/api/igdl?url=${text}`)
   let data = await res.json();
   const results = data.result
  conn.sendFile(m.chat, results.data[0], 'video.mp4', '', m);
