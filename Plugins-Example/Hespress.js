807bb87a403dc3f6aa57c242e7a2178789a81a00
MidSoune <75175804+midsoune@users.noreply.github.com>
2024-04-11 00:33:53 +0000

    Update Hespress.js

--- Plugins-Example/Hespress.js
+++ Plugins-Example/Hespress.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 const midsoune = async (m, {conn, args}) => {
   if (args[0] && args[0].startsWith('https://')) {
     try {
-      let res = await fetch(`https://midsoune-b5be75a7ee15.herokuapp.com/api/rhespress?lien=${args[0]}`);
+      let res = await fetch(`https://midsouneapi-fee7b0be8faf.herokuapp.com/api/rhespress?lien=${args[0]}`);
       let data = await res.json();
       const results = JSON.parse(data.result);
       let cap = `${results.content}`;
@@ -13,7 +13,7 @@ const midsoune = async (m, {conn, args}) => {
     }
   } else {
     try {
-      let res = await fetch('https://midsoune-b5be75a7ee15.herokuapp.com/api/hespress');
+      let res = await fetch('https://midsouneapi-fee7b0be8faf.herokuapp.com/api/hespress');
       let data = await res.json();
       const results = JSON.parse(data.result);
       let message = '';
