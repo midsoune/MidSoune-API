91b012565f3b29f657a2e515a688346cb0e87f7a
MidSoune <75175804+midsoune@users.noreply.github.com>
2024-04-11 00:38:44 +0000

    Update Youtube.js

--- Plugins-Example/Youtube.js
+++ Plugins-Example/Youtube.js
@@ -1,23 +1,18 @@
-// Video
 import fetch from 'node-fetch';
-const midsoune = async (m, {conn, text}) => {
-let res = await fetch(`https://midsouneapi-fee7b0be8faf.herokuapp.com/api/youtube?url=${text}`)
-  let data = await res.json();
-    const res = data.video.url
+const midsoune = async (m, {conn, text, command}) => {
+  if ( command === 'ytaa'){
+    let res = await fetch(`https://midsouneapi-fee7b0be8faf.herokuapp.com/api/youtube?url=${text}`)
+    let data = await res.json();
+    const videoURL = data.video.url
     const title = data.video.title
-  await conn.sendMessage(m.chat, { video: { url: res }, mimetype: 'video/mp4', fileName: title + '.mp4'}, { quoted: m })
-};
-midsoune.command = ['youtube', 'ytv'];
-export default midsoune;
-
-// Audio
-import fetch from 'node-fetch';
-const midsoune = async (m, { conn, text }) => {
-    let results = await fetch(`https://midsouneapi-fee7b0be8faf.herokuapp.com/api/youtube?url=${text}`);
-    let data = await results.json();
-    const res = data.audio.url
+    await conn.sendMessage(m.chat, { video: { url: videoURL }, mimetype: 'video/mp4', fileName: title + '.mp4'}, { quoted: m })
+  } else if (command === 'ytvv'){
+    let res = await fetch(`https://midsouneapi-fee7b0be8faf.herokuapp.com/api/youtube?url=${text}`);
+    let data = await res.json();
+    const audioURL = data.audio.url
     const title = data.audio.title
-    await conn.sendMessage(m.chat, { audio: { url: res }, mimetype: 'audio/mp4', fileName: `${title}.mp3`}, {quoted: m})
-};
-midsoune.command = ['ytmp3', 'yta'];
+    await conn.sendMessage(m.chat, { audio: { url: audioURL }, mimetype: 'audio/mp4', fileName: `${title}.mp3`}, {quoted: m})
+  }
+}
+midsoune.command = ['ytvv', 'ytaa'];
 export default midsoune;
